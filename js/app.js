1a5466ec122c3ccc29d9213b829dbd89116b2b0d
coenwsp <coen.rouppevandervoort@wsp.com>
2026-06-09 11:38:10 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+const map = L.map('map').setView(DEFAULT_VIEW.center, DEFAULT_VIEW.zoom);const map = L.map('map = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'OSM'
+}).addTo(map);
+
+// laad WMS lagen
+const overlayMaps = loadWMSLayers(map);
+
+// layer control
+L.control.layers({ "OSM": osm }, overlayMaps).addTo(map);
+``
+
+// base layer
 

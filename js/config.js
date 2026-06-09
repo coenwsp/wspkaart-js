9372672485945b5bce59bbb25e977a4cc5b6fd86
coenwsp <coen.rouppevandervoort@wsp.com>
2026-06-09 14:17:09 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,36 +11,23 @@ const MAP_LAYERS = [
   },
 
   {
-    type: "custom",
-    name: "G: Provincies",
-    layer: L.tileLayer.wms(
-      'https://service.pdok.nl/cbs/gebiedsindelingen/2023/wms/v1_0',
-      {
-        layers: 'provincie_gegeneraliseerd',
-        format: 'image/png',
-        transparent: true,
-        opacity: 0.5
-      }
-    )
+    type: "wms",
+    name: "Provincies",
+    url: "https://service.pdok.nl/cbs/gebiedsindelingen/2023/wms/v1_0",
+    layers: "provincie_gegeneraliseerd",
+    format: "image/png",
+    transparent: true,
+    opacity: 0.5
   },
 
   {
-    type: "custom",
-    name: "G: Gemeenten",
-    layer: L.layerGroup([
-      L.tileLayer.wms('https://service.pdok.nl/cbs/gebiedsindelingen/2023/wms/v1_0', {
-        layers: 'gemeente_gegeneraliseerd',
-        format: 'image/png',
-        transparent: true,
-        opacity: 0.6
-      }),
-      L.tileLayer.wms('https://service.pdok.nl/cbs/gebiedsindelingen/2023/wms/v1_0', {
-        layers: 'gemeente_labelpoint',
-        format: 'image/png',
-        transparent: true,
-        opacity: 0.8
-      })
-    ])
+    type: "wms",
+    name: "Gemeenten",
+    url: "https://service.pdok.nl/cbs/gebiedsindelingen/2023/wms/v1_0",
+    layers: "gemeente_gegeneraliseerd",
+    format: "image/png",
+    transparent: true,
+    opacity: 0.5
   }
 
 ];

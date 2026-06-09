53f5405f32fef422890642cdac494b47226c8421
coenwsp <coen.rouppevandervoort@wsp.com>
2026-06-09 11:35:48 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,21 @@
+// ✅ HIER voeg je al je WMS lagen toe// ✅ HIER voeg je al je_LAYERS = [
+  {
+    name: "AHN hoogte",
+    url: "https://service.pdok.nl/rws/ahn/wms/v1_0",
+    layers: "ahn3_05m_dtm",
+    format: "image/png"
+  },
+  {
+    name: "Waterkaart",
+    url: "https://geodata.nationaalgeoregister.nl/wateren/wms",
+    layers: "waterdeel",
+    format: "image/png"
+  }
+];
+
+// NIET aanpassen
+const DEFAULT_VIEW = {
+  center: [52.1, 5.1],
+  zoom: 8
+};
 

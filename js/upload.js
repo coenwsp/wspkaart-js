0ee8c4ed36575c0fef19c178f15862603982bd05
coenwsp <coen.rouppevandervoort@wsp.com>
2026-06-09 11:39:05 +0200

    Update upload.js

--- js/upload.js
+++ js/upload.js
@@ -1,1 +1,17 @@
+document.getElementById('uploadGeoJSON').addEventListener('change', function (e) {
 
+  const file = e.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function () {
+    const geojson = JSON.parse(reader.result);
+
+    const layer = L.geoJSON(geojson).addTo(map);
+
+    map.fitBounds(layer.getBounds());
+
+    currentBBox = layer.getBounds();
+  };
+
+  reader.readAsText(file);
+});

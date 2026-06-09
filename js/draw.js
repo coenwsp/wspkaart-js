ea1f4ee8e36d46873333d08cf71284ed1af2a440
coenwsp <coen.rouppevandervoort@wsp.com>
2026-06-09 11:38:47 +0200

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -1,1 +1,31 @@
+let drawnItems = new L.FeatureGroup();let drawnItems = new L.Feature drawnItems.addLayer(layer);
+
+  currentBBox = layer.getBounds();
+
+  console.log("BBOX:", currentBBox.toBBoxString());
+});
+map.addLayer(drawnItems);
+
+const drawControl = new L.Control.Draw({
+  draw: {
+    polygon: false,
+    polyline: false,
+    marker: false,
+    circle: false,
+    rectangle: true // ✅ alleen bbox
+  },
+  edit: {
+    featureGroup: drawnItems
+  }
+});
+
+map.addControl(drawControl);
+
+let currentBBox = null;
+
+// wanneer bbox getekend wordt
+map.on(L.Draw.Event.CREATED, function (e) {
+  drawnItems.clearLayers();
+
+  const layer = e.layer;
 

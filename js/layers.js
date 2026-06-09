e7cbb69350a96382059d26521405d9722e1de967
coenwsp <coen.rouppevandervoort@wsp.com>
2026-06-09 13:34:54 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -4,12 +4,12 @@ function loadWMSLayers(map) {
   WMS_LAYERS.forEach(layer => {
 
     const wmsLayer = L.tileLayer.wms(layer.url, {
-  layers: layer.layers,
-  format: layer.format,
-  transparent: true,
-  version: '1.3.0',
-  crs: L.CRS.EPSG3857
-});
+      layers: layer.layers,
+      format: layer.format,
+      transparent: true,
+      version: '1.3.0',
+      crs: L.CRS.EPSG3857
+    });
 
     overlays[layer.name] = wmsLayer;
   });

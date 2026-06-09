ee776d7c7f470ab6c29562b1a3386e2636bfef14
coenwsp <coen.rouppevandervoort@wsp.com>
2026-06-09 11:38:28 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,1 +1,16 @@
+function loadWMSLayers(map) {
+  const overlays = {};
 
+  WMS_LAYERS.forEach(layer => {
+
+    const wmsLayer = L.tileLayer.wms(layer.url, {
+      layers: layer.layers,
+      format: layer.format,
+      transparent: true
+    });
+
+    overlays[layer.name] = wmsLayer;
+  });
+
+  return overlays;
+}
